7e9032c43d945d363253a228bddedd58d6c3212d
DBJ-git <divyjoshi.9999@gmail.com>
2025-09-06 17:01:41 +0530

    Update sum_N.c

--- sum_N.c
+++ sum_N.c
@@ -4,7 +4,7 @@ int main() {
     int n;
     int sum = 0;
 
-    printf("Enter a number: ");
+    printf("Enter how many nautral number's sum you want : ");
     scanf("%d", &n);
 
     for(int i = 1 ; i <= n ; i++){
